55a1180c890bbfe70ece63363d9b9fcf9b900191
Eoin Wickens <eoin.wickens@mycit.ie>
2018-11-23 12:34:14 +0000

    Added Java script

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner kb = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        menu();
+    }
+
+    public static int add(int i, int j){
+        return i+j;
+    }
+    public static int subtract(int i, int j){
+        return i-j;
+    }
+    public static void menu(){
+      add(1,2);
+      subtract (5,4);
+        }
+
+}

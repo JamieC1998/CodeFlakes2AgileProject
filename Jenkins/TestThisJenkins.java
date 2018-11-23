8345dd103f5a4c152de267aab94a4b182f023376
Thomas Crombie (R00155348) <thomas.crombie@mycit.ie>
2018-11-23 12:46:00 +0000

    added a java file for Jenkins to run a JUnit test on

---
+++ Jenkins/TestThisJenkins.java
@@ -0,0 +1,16 @@
+package blah;
+
+public class TestThisJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(doSomething(5,5));
+		System.out.println(doSomething(15,2));
+		System.out.println(doSomething(2,99));
+	}
+
+	public static int doSomething(int numberOne, int numberTwo) {
+		return numberOne * numberTwo;
+	}
+	
+}
